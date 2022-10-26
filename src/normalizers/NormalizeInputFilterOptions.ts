e71ebc33d1351a1d23957e0c8b68ef50c2730a80
Phillippe Santana <phillippelevidad@gmail.com>
2022-10-26 10:11:32 -0300

    Moved interface to own file

---
+++ src/normalizers/NormalizeInputFilterOptions.ts
@@ -0,0 +1,28 @@
+/**
+ * Opções de normalização de filtros.
+ */
+export interface NormalizeInputFilterOptions {
+  /**
+   * Lista de campos que serão aceitos, isto é,
+   * mantidos no filtro. Tem prioridade sobre os campos
+   * ignorados.
+   */
+  acceptedFields?: string[];
+  /**
+   * Lista de campos que serão ignorados, isto é,
+   * removidos do filtro. Só funciona se não especificar
+   * campos aceitos.
+   */
+  ignoredFields?: string[];
+  /**
+   * Profundidade máxima do filtro.
+   * O que aumenta a profundidade de um filtro são os operadores lógicos: AND e OR.
+   * O valor padrão é 3. Itens mais profundos serão removidos.
+   */
+  maxDepth?: number;
+  /**
+   * Profundidade máxima para os valores dos campos.
+   * O valor padrão é 3. Valores mais profundos serão removidos.
+   */
+  fieldValueMaxDepth?: number;
+}
--- src/normalizers/normalizeInputFilter.ts
+++ src/normalizers/normalizeInputFilter.ts
@@ -13,35 +13,7 @@ import {
 import { flattenLogicalOperators } from "./flattenLogicalOperators";
 import { limitToDepth } from "./limitToDepth";
 import { normalizeFilterValue } from "./normalizeFilterValue";
-
-/**
- * Opções de normalização de filtros.
- */
-export interface NormalizeInputFilterOptions {
-  /**
-   * Lista de campos que serão aceitos, isto é,
-   * mantidos no filtro. Tem prioridade sobre os campos
-   * ignorados.
-   */
-  acceptedFields?: string[];
-  /**
-   * Lista de campos que serão ignorados, isto é,
-   * removidos do filtro. Só funciona se não especificar
-   * campos aceitos.
-   */
-  ignoredFields?: string[];
-  /**
-   * Profundidade máxima do filtro.
-   * O que aumenta a profundidade de um filtro são os operadores lógicos: AND e OR.
-   * O valor padrão é 3. Itens mais profundos serão removidos.
-   */
-  maxDepth?: number;
-  /**
-   * Profundidade máxima para os valores dos campos.
-   * O valor padrão é 3. Valores mais profundos serão removidos.
-   */
-  fieldValueMaxDepth?: number;
-}
+import { NormalizeInputFilterOptions } from "./NormalizeInputFilterOptions";
 
 type KeyValuePair = {
   key: string;
