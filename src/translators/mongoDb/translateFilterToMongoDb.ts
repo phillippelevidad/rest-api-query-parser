fe2798df64abb05bb12cfcdfc471e1a73a012791
Phillippe Santana <phillippelevidad@gmail.com>
2022-10-28 11:32:14 -0300

    Ability to remap field names for mongo

--- src/translators/mongoDb/translateFilterToMongoDb.ts
+++ src/translators/mongoDb/translateFilterToMongoDb.ts
@@ -1,4 +1,5 @@
 import { Document, Filter } from "mongodb";
+import { Concrete } from "../../helpers/Concrete";
 import { FilterNode } from "../../models/FilterNode";
 import { FilterTree } from "../../models/FilterTree";
 import { LogicalNode } from "../../models/LogicalNode";
@@ -7,39 +8,68 @@ import { LOGICAL_OPERATORS } from "../../models/operators";
 import { mergeDuplicatedFields } from "./mergeDuplicatedFields";
 import { translateOperator } from "./translateOperator";
 
+export interface TranslateFilterOptions {
+  remapFieldNames?: Record<string, string>;
+}
+
+export function normalizeFilterOptions(
+  options?: TranslateFilterOptions
+): Concrete<TranslateFilterOptions> {
+  return {
+    remapFieldNames: {},
+    ...options,
+  };
+}
+
 /**
  * Converte um FilterTree em um objeto que pode ser usado como filtro no MongoDB.
  * @param filter A árvore de filtros a ser convertida.
  * @returns Um objeto que pode ser usado como filtro no MongoDB.
  */
-export function translateFilterToMongoDb(filter: FilterTree): Filter<Document> {
+export function translateFilterToMongoDb(
+  filter: FilterTree,
+  options?: TranslateFilterOptions
+): Filter<Document> {
   if (filter.length === 0) return {};
-  const translated = translateNode(filter[0]) ?? {};
+  const opt = normalizeFilterOptions(options);
+  const translated = translateNode(filter[0], opt) ?? {};
   const merged = mergeDuplicatedFields(translated);
   return merged;
 }
 
-function translateNode(node: LogicalOrFilterNode): Filter<Document> | null {
+function translateNode(
+  node: LogicalOrFilterNode,
+  options: Concrete<TranslateFilterOptions>
+): Filter<Document> | null {
   return LOGICAL_OPERATORS.includes(node.operator)
-    ? translateLogicalNode(node as LogicalNode)
-    : translateFilterNode(node as FilterNode);
+    ? translateLogicalNode(node as LogicalNode, options)
+    : translateFilterNode(node as FilterNode, options);
 }
 
-function translateLogicalNode(node: LogicalNode): Filter<Document> | null {
+function translateLogicalNode(
+  node: LogicalNode,
+  options: Concrete<TranslateFilterOptions>
+): Filter<Document> | null {
   const mongoOperator = translateOperator(node.operator);
   if (mongoOperator === null) return null;
   return {
     [mongoOperator]: node.conditions
-      .map(translateNode)
+      .map((node) => translateNode(node, options))
       .filter((entry) => entry !== null) as Filter<Document>[],
   };
 }
 
-function translateFilterNode(node: FilterNode): Filter<Document> | null {
+function translateFilterNode(
+  node: FilterNode,
+  options: Concrete<TranslateFilterOptions>
+): Filter<Document> | null {
   const mongoOperator = translateOperator(node.operator);
   if (mongoOperator === null) return null;
+
+  const fieldName = options.remapFieldNames[node.field] ?? node.field;
+
   return {
-    [node.field]: {
+    [fieldName]: {
       [mongoOperator]: node.value,
     },
   };
